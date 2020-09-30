5d6d43bebff26d22a3f99e609d539fa3b0d5b0be
Migracode <sanchezgalejandroj@gmail.com>
2020-09-30 18:21:27 +0200

    test number 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const formidable = require('express-formidable');
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 80;
 
 app.use(cors());
 
